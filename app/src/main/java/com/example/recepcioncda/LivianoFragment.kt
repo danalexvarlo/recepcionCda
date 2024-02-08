b36a82c5159ce02602fae0a58be3d57fdd3165c4
danalexvarlo <danalexvarl@gmail.com>
2024-02-08 16:58:17 -0500

    inicio del fragmento presiones de livianos

---
+++ app/src/main/java/com/example/recepcioncda/LivianoFragment.kt
@@ -0,0 +1,55 @@
+package com.example.recepcioncda
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.recepcioncda.R.*
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+class LivianoFragment: Fragment()
+{
+    lateinit var toggle : ActionBarDrawerToggle
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_liviano, container, false)
+
+        val toolbar: Toolbar = view.findViewById(R.id.toolbar_liviano)
+        val drawerLayout : DrawerLayout = view.findViewById(R.id.livianoFragment)
+
+        toggle = ActionBarDrawerToggle(this.requireContext() as AppCompatActivity,
+            drawerLayout, toolbar, string.open, string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        toolbar.setNavigationOnClickListener{
+            if(drawerLayout.isDrawerOpen((GravityCompat.START))){
+                drawerLayout.closeDrawer(GravityCompat.START)
+            }
+            else{
+                drawerLayout.openDrawer(GravityCompat.START)
+            }
+        }
+
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
+
